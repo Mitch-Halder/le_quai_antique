1c5d4f5dcad216521db5960ff7e826726ca59d04
Mitch <halder.jean-michel@orange.fr>
2023-09-03 16:22:47 +0200

    Add files via upload

---
+++ Le-Quai-Antique-test-3/script3.js
@@ -0,0 +1,18 @@
+const toggler = document.querySelector(".hamburger");
+const navLinksContainer = document.querySelector(".navlinks-container");
+const toggleNav = e => {
+    toggler.classList.toggle("open");
+    const ariaToggle = toggler.getAttribute("aria-expanded") === "true" ? "false" : "true";
+    toggler.setAttribute("aria-expanded", ariaToggle);
+    navLinksContainer.classList.toggle("open");
+};
+
+toggler.addEventListener("click", toggleNav);
+
+new ResizeObserver (entries => {
+    if (entries[0].contentRect.width <= 900){
+        navLinksContainer.computedStyleMap.transition = "transform 0.3s ease-out";
+    } else {
+        navLinksContainer.computedStyleMap.transition = "none";
+    }
+}).observe (document.body)
